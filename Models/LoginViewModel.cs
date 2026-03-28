bdc40ce4b398ad0765e777c60af77358c5425a6f
fernandoprincipecuellar <fernandoprincipe20.a@gmail.com>
2026-03-27 22:32:48 -0500

    cambio fer

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Grupo_gris.Models;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "El correo es obligatorio")]
+    [EmailAddress(ErrorMessage = "Ingresa un correo válido")]
+    [Display(Name = "Correo electrónico")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "La contraseña es obligatoria")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Contraseña")]
+    public string Password { get; set; } = string.Empty;
+
+    public string ReturnUrl { get; set; } = "/";
+}
