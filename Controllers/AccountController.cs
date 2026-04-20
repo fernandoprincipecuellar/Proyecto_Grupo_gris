3b2b91a7e30476c6c75bb75559025e886e00700c
Albert <sanchescastiyomarta@gmail.com>
2026-04-19 22:10:32 -0500

    agregado el register

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -65,4 +65,42 @@ public class AccountController : Controller
         await _signInManager.SignOutAsync();
         return RedirectToAction("Index", "Home");
     }
+
+
+[HttpGet]
+public IActionResult Register()
+{
+    return View();
+}
+
+[HttpPost]
+[ValidateAntiForgeryToken]
+public async Task<IActionResult> Register(RegisterViewModel model)
+{
+    if (!ModelState.IsValid)
+    {
+        return View(model);
+    }
+
+    var user = new IdentityUser
+    {
+        UserName = model.Email,
+        Email = model.Email
+    };
+
+    var result = await _userManager.CreateAsync(user, model.Password);
+
+    if (result.Succeeded)
+    {
+        await _signInManager.SignInAsync(user, isPersistent: false);
+        return RedirectToAction("Index", "Home");
+    }
+
+    foreach (var error in result.Errors)
+    {
+        ModelState.AddModelError(string.Empty, error.Description);
+    }
+
+    return View(model);
+}
 }
---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Grupo_gris.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Nombre completo")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
